e8216b999810c01d6b4221ebd1b5d4dea4fffe01
Shi Lei <shi_lei@massclouds.com>
2023-09-28 03:18:36 +0000

    v0: first line in kernel

---
+++ server/src/main.rs
@@ -0,0 +1,15 @@
+//! v0: first line in kernel.
+
+#![no_std]
+#![no_main]
+
+mod lang_items;
+
+#[no_mangle]
+#[link_section = ".text.boot"]
+unsafe extern "C" fn _start() -> ! {
+    core::arch::asm!(
+        "wfi",
+        options(noreturn)
+    )
+}
