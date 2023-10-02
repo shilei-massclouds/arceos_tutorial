97905fe064f72fa82f1b621b19080c2ba99d71c1
Shi Lei <shi_lei@massclouds.com>
2023-10-02 02:40:15 +0000

    v3.0: add axstd and support println!

--- axorigin/src/main.rs
+++ axorigin/src/main.rs
@@ -1,13 +1,10 @@
 #![no_std]
 #![no_main]
 
-extern crate alloc;
-extern crate axruntime;
-
-use alloc::string::String;
+use axstd::{println, String};
 
 #[no_mangle]
 pub fn main() {
-    let s = String::from("\nHello, ArceOS![from String]\n");
-    axhal::console::write_bytes(s.as_bytes());
+    let s = String::from("Hello, ArceOS!");
+    println!("\n{s} Now axstd is okay!");
 }
---
+++ axstd/src/io.rs
@@ -0,0 +1,19 @@
+/// Constructs a new handle to the standard output of the current process.
+
+use core::fmt::{Write, Error};
+use axsync::BootCell;
+
+struct StdoutRaw;
+
+impl Write for StdoutRaw {
+    fn write_str(&mut self, s: &str) -> Result<(), Error> {
+        axhal::console::write_bytes(s.as_bytes());
+        Ok(())
+    }
+}
+
+static STDOUT: BootCell<StdoutRaw> = unsafe { BootCell::new(StdoutRaw) };
+
+pub fn __print_impl(args: core::fmt::Arguments) {
+    STDOUT.access().write_fmt(args).unwrap();
+}
